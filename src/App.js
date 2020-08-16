1f115af5e7efa38dad3982909f5d598df09ac023
ishai2s4 <ishai2s4@gmail.com>
2020-08-16 19:21:18 +0530

    mt first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hey isha sharma</h1>
+    <h2>hbdrf</h2>
+</>
+  );
+}
+
+export default App;
